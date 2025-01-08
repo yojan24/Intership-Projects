ef777f2397734195b400dc4160454e924d89d5ae
YojanPawade <pawadeyojan487@gmail.com>
2025-01-08 17:42:21 +0530

    done

--- Attendance Tracker/Front-end/src/Redux/constant.js
+++ Attendance Tracker/Front-end/src/Redux/constant.js
@@ -1,3 +1,3 @@
-const api = "";
-export const BASE_URL = ``;
+const api = "https://attendance-tracking-system-gici.onrender.com";
+export const BASE_URL = `${api}`;
 export const USER_URL = "/api/user";
