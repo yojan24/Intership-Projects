8bdcc55b0357d5d4a36c747f767f393283e10822
YojanPawade <pawadeyojan487@gmail.com>
2025-01-08 17:12:07 +0530

    done

--- Attendance Tracker/Backend/index.js
+++ Attendance Tracker/Backend/index.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: process.env.origin,
+    origin: process.env.origin || "http://localhost:5173/",
     methods: ["GET", "POST", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
